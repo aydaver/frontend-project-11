ba48e3746f51e1ca32229c24c99744d1b1d34936
aydaver <Aydavercheck@gmail.com>
2025-03-28 12:15:02 +0500

    vercel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-unresolved */
 import './styles/styles.scss';
 import 'bootstrap';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import app from './model/app.js';
 
 app();
